b489f98044ae8c8a69b15603cc02a69f7831c002
PavelDermichev1998 <pdermichev@gmail.com>
2022-01-25 11:58:22 +0300

    localStorage useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {Tablo} from "./Tablo/Tablo";
 import {Buttons} from "./Buttons/Buttons";
@@ -7,13 +7,34 @@ import {useState} from "react";
 
 function App() {
 
-    let [count, setCount] = useState(0)
+    let [count, setCount] = useState<number>(0)
+
+    useEffect( () => {
+        let countAcString = localStorage.getItem('getKey')
+        if (countAcString) {
+            let newCount = JSON.parse(countAcString)
+            setCount(newCount)
+        }
+    }, [])
+
+    useEffect( () => {
+        localStorage.setItem('getKey', JSON.stringify(count))
+    }, [count])
+
+
+    const clearLocalStorage = () => {
+        localStorage.clear()
+        setCount(0)
+    }
+
+
 
     return (
         <div className="App">
             <div className="Counter">
                 <Tablo count={count}/>
                 <Buttons setCount={setCount} count={count}/>
+                <button onClick={clearLocalStorage}>clearLocalStorage</button>
             </div>
         </div>
     );
